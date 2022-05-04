f1f350d86ea1472930c436b6df97aba2c89d0692
Steph Davidson <stephcdavidson@gmail.com>
2022-05-04 00:16:11 -0400

    branchin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 console.clear();
 
+// import { HDRCubeTextureLoader } from 'https://cdn.jsdelivr.net/npm/three@0.114.0/examples/jsm/loaders/HDRCubeTextureLoader.js';
+// import { RoughnessMipmapper } from "https://cdn.jsdelivr.net/npm/three@0.113.0/examples/jsm/utils/RoughnessMipmapper.js";
+import { RGBELoader } from "https://cdn.jsdelivr.net/npm/three@0.113.0/examples/jsm/loaders/RGBELoader.js";
+
 //====================================================== canvas
 
 const model1 = document.querySelector("#model1");
@@ -16,28 +20,16 @@ camera.position.z = 5;
 camera.position.y = 0;
 
 //===================================================== lights
-var light2 = new THREE.DirectionalLight(0xefefff, 1);
-light2.position.set(-4, 4, 0).normalize();
-scene.add(light2);
-var light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, -4, 4).normalize();
-scene.add(light);
-
-//===================================================== resize
-window.addEventListener("resize", function () {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-});
+// var light2 = new THREE.DirectionalLight(0xefefff, 1);
+// light2.position.set(-4, 4, 0).normalize();
+// scene.add(light2);
+// var light = new THREE.DirectionalLight(0xffffff, 1);
+// light.position.set(0, -4, 4).normalize();
+// scene.add(light);
 
 //===================================================== model
-var loader = new THREE.GLTFLoader();
-var mixer;
 var model;
-// loader.load("https://assets.codepen.io/246465/smaller.gltf", function (gltf) {
-loader.load("./v3.gltf", function (gltf) {
+new THREE.GLTFLoader().load("./v3.gltf", function (gltf) {
     gltf.scene.traverse(function (node) {
         if (node instanceof THREE.Mesh) {
             node.castShadow = true;
@@ -50,6 +42,32 @@ loader.load("./v3.gltf", function (gltf) {
     scene.add(model);
 });
 
+// ==================================================== hdr
+const params = {
+    exposure: 2.0,
+};
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = params.exposure;
+renderer.outputEncoding = THREE.sRGBEncoding;
+new RGBELoader().load("https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/equirectangular/royal_esplanade_1k.hdr"),
+    function (texture) {
+        const material = new THREE.MeshBasicMaterial({ map: texture });
+        const quad = new THREE.PlaneGeometry((1.5 * textureData.width) / textureData.height, 1.5);
+        const mesh = new THREE.Mesh(quad, material);
+        scene.add(mesh);
+        render();
+    };
+//===================================================== resize
+window.addEventListener("resize", function () {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
+
+//@@@@@@@@@@@@@@@@@@@@@@@@scroll trigger@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@scroll trigger@@@@@@@@@@@@@@@@@@@@@@
 //@@@@@@@@@@@@@@@@@@@@@@@@scroll trigger@@@@@@@@@@@@@@@@@@@@@@
 gsap.registerPlugin(ScrollTrigger);
 
